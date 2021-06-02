c1e142adc807bdee82d91c7b5bd9c1b75b286c5e
Pratik Karki <pratikkarki5@gmail.com>
2021-06-02 08:10:29 +0545

    Final Changes

--- src/api/YoutubeConfig.js
+++ src/api/YoutubeConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDGEwklPLzGItmAdhM8O6dmqkDvxFOmQUY';
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
